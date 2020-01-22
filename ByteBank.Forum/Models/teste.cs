f41222a432c5a1bac5c2a2c7b180797fabbfdd87
Raul Guilherme Demonte Pontes <raul.pontes@microserviceit.com.br>
2020-01-22 11:24:04 -0300

    Primeiro push

---
+++ ByteBank.Forum/Models/teste.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ByteBank.Forum.Models
+{
+    public static class teste
+    {
+        public static bool Verificar<T>(T param)
+        {
+            if(param.GetType() == Int)
+            return false;
+        }
+    }
+}
---
+++ ByteBank.Forum/ViewModels/ContaRegistrarViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class ContaRegistrarViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Nome completo")]
+        public string NomeCompleto { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar senha")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
